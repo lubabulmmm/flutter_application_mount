c99d761eab03b7f0b00dc37de39ad0db5f2d13a6
lubabulmmm <lubabul.ilmi16@gmail.com>
2023-12-21 05:08:48 +0700

    update
    update profil gunung

--- lib/screens/home/daftargunung.dart
+++ lib/screens/home/daftargunung.dart
@@ -49,90 +49,146 @@ class _DaftarGunungState extends State<DaftarGunung> {
                   itemBuilder: (context, index) {
                     final DocumentSnapshot documents =
                         streamSnapshot.data!.docs[index];
+
+                    Future<dynamic> addToFavorite() async {
+                      FirebaseAuth _auth = FirebaseAuth.instance;
+                      var currentUser = _auth.currentUser;
+                      CollectionReference _collectionReference =
+                          FirebaseFirestore.instance.collection('fav-user');
+
+                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                          content: Text('Berhasil Menambahkan ke Favorit')));
+
+                      return _collectionReference
+                          .doc(currentUser!.email)
+                          .collection('items')
+                          .doc()
+                          .set({
+                        "nama": documents['nama'],
+                        "lokasi": documents['lokasi'],
+                        "pict": documents['pict'],
+                        "desc": documents['desc'],
+                        "jam": documents['jam'],
+                        "tiket": documents['tiket']
+                      }).then((value) => print('Ditambahkan ke favorit'));
+                    }
+
                     return GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => DetailScreen(
-                              nama: documents['nama'],
-                              tiket: documents['tiket'],
-                              jam: documents['jam'],
-                              desc: documents['desc'],
-                              pict: documents['pict'],
-                              lokasi: documents['lokasi'],
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => DetailScreen(
+                                nama: documents['nama'],
+                                tiket: documents['tiket'],
+                                jam: documents['jam'],
+                                desc: documents['desc'],
+                                pict: documents['pict'],
+                                lokasi: documents['lokasi'],
+                              ),
                             ),
-                          ),
-                        );
-                      },
-                      child: Card(
-                        color: const Color(0xFF0F1A1A),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: Row(
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.all(10.0),
-                                child: Container(
-                                  width: 150,
-                                  height: 150,
-                                  child: ClipRRect(
-                                      borderRadius: BorderRadius.circular(40),
-                                      child: Image.network(
-                                          "https://drive.google.com/uc?export=view&id=${documents['pict']}")),
-                                ),
+                          );
+                        },
+                        child: Card(
+                            color: const Color(0xFF0F1A1A),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
                               ),
-                              SizedBox(width: 30),
-                              Expanded(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        Text(
-                                          documents['nama'],
-                                          style: const TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 20,
-                                            fontFamily: 'Montserrat',
-                                            color: Colors.white,
+                              child: Row(children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: Container(
+                                    width: 150,
+                                    height: 150,
+                                    child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(30),
+                                        child: Image.network(
+                                            "https://drive.google.com/uc?export=view&id=${documents['pict']}")),
+                                  ),
+                                ),
+                                SizedBox(width: 20),
+                                Expanded(
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            documents['nama'],
+                                            style: const TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 20,
+                                              fontFamily: 'Montserrat',
+                                              color: Colors.white,
+                                            ),
                                           ),
-                                        ),
-                                      ],
-                                    ),
-                                    SizedBox(
-                                      height: 10,
-                                    ),
-                                    Row(
-                                      children: [
-                                        const Icon(
-                                          Icons.location_on_outlined,
-                                          color: Colors.grey,
-                                        ),
-                                        const SizedBox(width: 10),
-                                        Text(
-                                          documents['lokasi'],
-                                          style: const TextStyle(
-                                            fontFamily: 'Poppins',
-                                            fontWeight: FontWeight.w300,
+                                          Spacer(),
+                                          StreamBuilder(
+                                            stream: FirebaseFirestore.instance
+                                                .collection('fav-user')
+                                                .doc(FirebaseAuth.instance
+                                                    .currentUser!.email)
+                                                .collection('items')
+                                                .where('nama',
+                                                    isEqualTo:
+                                                        documents['nama'])
+                                                .snapshots(),
+                                            builder: (context,
+                                                AsyncSnapshot snapshot) {
+                                              return MouseRegion(
+                                                cursor:
+                                                    SystemMouseCursors.click,
+                                                child: IconButton(
+                                                  icon: snapshot.data.docs
+                                                              .length ==
+                                                          0
+                                                      ? Icon(
+                                                          Icons
+                                                              .favorite_outline,
+                                                          color:
+                                                              Colors.red[800],
+                                                        )
+                                                      : Icon(Icons.favorite,
+                                                          color:
+                                                              Colors.red[800]),
+                                                  color: Colors.red,
+                                                  onPressed: addToFavorite,
+                                                ),
+                                              );
+                                            },
+                                          ),
+                                        ],
+                                      ),
+                                      SizedBox(
+                                        height: 10,
+                                      ),
+                                      Row(
+                                        children: [
+                                          const Icon(
+                                            Icons.location_on_outlined,
                                             color: Colors.grey,
-                                            fontSize: 13,
                                           ),
-                                        ),
-                                      ],
-                                    ),
-                                  ],
+                                          const SizedBox(width: 10),
+                                          Text(
+                                            documents['lokasi'],
+                                            style: const TextStyle(
+                                              fontFamily: 'Poppins',
+                                              fontWeight: FontWeight.w300,
+                                              color: Colors.grey,
+                                              fontSize: 13,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
                                 ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    );
+                              ]),
+                            )));
                   },
                 );
               }
--- lib/screens/home/profil.dart
+++ lib/screens/home/profil.dart
@@ -76,16 +76,17 @@ class _ProfilScreenState extends State<ProfilScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          automaticallyImplyLeading: false,
-          backgroundColor: Color.fromARGB(255, 98, 62, 1),
-          centerTitle: true,
-          title: Text(
-            'Profil',
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold, // Make the text bold
-            ),
-          )),
+        title: const Text(
+          'Profil',
+          style: TextStyle(
+            color: Color.fromARGB(255, 255, 255, 255),
+            fontFamily: 'Poppins',
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 6, 0, 83),
+      ),
       backgroundColor: Colors.black,
       body: Center(
         child: Column(
@@ -198,12 +199,12 @@ class _DemoBottomAppBar extends StatelessWidget {
       height: isVisible ? 80.0 : 0,
       child: BottomAppBar(
         elevation: isElevated ? null : 0.0,
-        color: Color.fromARGB(255, 84, 65, 1),
+        color: const Color.fromARGB(255, 6, 0, 83),
         child: Row(
           mainAxisAlignment:
               MainAxisAlignment.spaceAround, // Center buttons with space around
           children: <Widget>[
-            // PROFIL
+            //! PROFIL
             IconButton(
               tooltip: 'Profil Kamu',
               icon: const Icon(
@@ -219,9 +220,9 @@ class _DemoBottomAppBar extends StatelessWidget {
             // SPACER
             Spacer(),
 
-            // BOOKMARK
+            //! FAVORITE
             IconButton(
-              tooltip: 'Suka',
+              tooltip: 'Favorit',
               icon: const Icon(Icons.favorite_border_outlined,
                   color: Colors.white),
               onPressed: () => Navigator.of(context).push(MaterialPageRoute(
@@ -231,7 +232,7 @@ class _DemoBottomAppBar extends StatelessWidget {
             // SPACER
             Spacer(),
 
-            // HOME
+            //! HOME
             IconButton(
               tooltip: 'Home',
               icon: const Icon(Icons.home_filled, color: Colors.white),
